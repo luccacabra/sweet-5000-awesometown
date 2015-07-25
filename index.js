bfd1f6c4acb7cadf07ffce0ae9bc595e7b0cf290
Jessica Lucci <jlucci@rmn.com>
2015-07-25 14:17:59 -0500

    adding in arduino

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ var $ = require('jquery');
 var express = require('express');
 var helpers = require('./lib/utils/helpers.js');
 var weather = require('./lib/weather/weather.js');
+var worker = require('./lib/worker.js');
 
 var app = express();
 
 app.get('/city/:cityId', function (req, res) {
     var cityData = weather.WeatherObj.getCity(req.params.cityId);
     var weatherIcon = helpers.parseCode(cityData);
-    res.send(helpers.weatherMapping(weatherIcon));
+    var weatherCode = helpers.weatherMapping(weatherIcon);
+    worker.weatherStation(weatherCode);
+    res.send(weatherCode);
 });
 
 var server = app.listen(3000, function () {
